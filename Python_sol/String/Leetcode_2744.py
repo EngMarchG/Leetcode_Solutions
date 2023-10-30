776dd068c7410f6cc8154b6161e699907b1a3662
EngMarchG <marchellino.ghorayeb@gmail.com>
2023-10-30 20:43:47 +0100

    'Updates'

---
+++ Python_sol/String/Leetcode_2744.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        max_pairs = 0
+        for i in range(len(words)-1):
+            for j in range(i+1, len(words)):
+                if words[i][0] == words[j][1] and words[i][1] == words[j][0]:
+                    max_pairs += 1
+        return max_pairs
+    
+# Slower solution since it checks the whole string
+class Solution:
+    def maximumNumberOfStringPairs(self, words: List[str]) -> int:
+        max_pairs = 0
+        for i in range(len(words)-1):
+            temp = 0
+            for j in range(i+1, len(words)):
+                if words[i] == words[j][::-1]:
+                    max_pairs += 1
+        return max_pairs
