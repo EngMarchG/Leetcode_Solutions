1aae38cc992ef548f806a86e0062a15a7028af50
EngMarchG <marchellino.ghorayeb@gmail.com>
2024-06-05 15:00:07 +0200

    'Updates'

--- Python_sol/Arrays_hashing/Leetcode_1002.py
+++ Python_sol/Arrays_hashing/Leetcode_1002.py
@@ -21,3 +21,29 @@ class Solution:
             if all(letter in word for word in words):
                 ans.extend([letter] * min(word.count(letter) for word in words))
         return ans
+    
+
+# Solution without relying on python specific functions
+class Solution:
+    # More efficient solution
+    def commonChars(self, words: List[str]) -> List[str]:
+        hmap = {}
+
+        for i, word in enumerate(words):
+            temp = {}
+            for letter in word:
+                temp[letter] = temp.get(letter, 0) + 1
+            
+            if not hmap:
+                if i != 0:
+                    return []
+                hmap = temp
+            else:
+                temp2 = {}
+                for key in temp.keys():
+                    if hmap.get(key, 0):
+                        temp2[key] = min(hmap[key], temp[key])
+                
+                hmap = temp2
+        
+        return [key for key in hmap.keys() for val in range(hmap[key])] 
