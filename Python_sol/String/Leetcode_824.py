3148caf1e6788d843912a7f6ad6bc4674d2bd47d
EngMarchG <marchellino.ghorayeb@gmail.com>
2025-05-20 00:01:45 +0200

    Update

--- Python_sol/String/Leetcode_824.py
+++ Python_sol/String/Leetcode_824.py
@@ -1,3 +1,17 @@
 class Solution:
     def toGoatLatin(self, sentence: str) -> str:
+        vowels = ['a', 'e', 'i', 'o', 'u']
+        ans = []
+        letters = 0
+
+        for word in sentence.split(" "):
+            letters += 1
+            if word[0].lower() in vowels:
+                temp = word + "ma"
+            else:
+                temp = word[1:] + word[0] + "ma"
+            
+            ans.append(temp + "a" * letters)
         
+        return " ".join(ans)
+
