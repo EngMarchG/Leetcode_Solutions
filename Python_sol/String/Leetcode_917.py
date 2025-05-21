1afc2f2c597aa7df6d08d8457eb355aa7fe7b877
EngMarchG <marchellino.ghorayeb@gmail.com>
2025-05-21 23:53:37 +0200

    Update

---
+++ Python_sol/String/Leetcode_917.py
@@ -0,0 +1,20 @@
+class Solution:
+    def reverseOnlyLetters(self, s: str) -> str:
+        left = 0
+        right = len(s)-1
+        temp = [0] * len(s)
+
+        while left <= right:
+            while not s[left].isalpha() and left < right:
+                temp[left] = s[left]
+                left += 1
+            while not s[right].isalpha() and right > left:
+                temp[right] = s[right]
+                right -= 1
+            temp[right] = s[left]
+            temp[left] = s[right]
+            left += 1
+            right -= 1
+        return "".join(temp)
+            
+            
